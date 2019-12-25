bb218e672d0db5a60d412109bb578ac75869703b
yanashayev <yana.shayev123@gmail.com>
2019-12-25 22:15:18 +0200

    update gui class

--- src/GUI/Graph_GUI.java
+++ src/GUI/Graph_GUI.java
@@ -2,7 +2,37 @@ package GUI;
 import utils.*;
 import java.awt.*;
 import javax.swing.*;
+import algorithms.*;
+import dataStructure.*;
+
 public class Graph_GUI {
-    JFrame frame= new JFrame(" draw graph");
-    
+    public static void main(String arg[]) {
+        int width=1400;
+        int height=1400;
+        int r=200;
+        DGraph g = new DGraph();
+        Range rx=new Range(-10 ,10);
+        Range ry=new Range(-10,10);
+        DrawGraph(width,height,rx,ry,r,g);
+
+    }
+    public static void DrawGraph(int width, int height, Range rx, Range ry, int resolution, graph g) {
+        JFrame frame = new JFrame(" draw graph");
+        frame.setSize(width,height);
+        StdDraw.setCanvasSize(width, height);
+        StdDraw.setXscale(rx.get_min(), rx.get_max());
+        StdDraw.setYscale(ry.get_min(), ry.get_max());
+        double delta = rx.get_max()-rx.get_min()/resolution;
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
